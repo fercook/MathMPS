e4c7113f60c857910801bbc13ab3841529d656ea
Fernando Cucchietti <fercook@gmail.com>
2010-02-18 11:20:12 +0100

    Created iPEPS left and right product

---
+++ parameters.m
@@ -0,0 +1,18 @@
+(* ::Package:: *)
+
+bond=15;
+length=20;
+intrange=2;
+
+
+outputfile="bond20length60range10";
+
+
+\[Mu]ini=0.0;
+\[Mu]end=-3.0;
+\[Mu]points=30;
+
+
+Jini=0.0;
+Jend=2.0;
+Jpoints=20;
